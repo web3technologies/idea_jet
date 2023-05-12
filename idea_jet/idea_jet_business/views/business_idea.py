a833ea10169ec704c691c34f94fdd009cd5385f0
zach <zach@web3technologies.io>
2023-05-12 14:01:19 -0500

    add is authenticated to the endpoint

--- idea_jet/idea_jet_business/views/business_idea.py
+++ idea_jet/idea_jet_business/views/business_idea.py
@@ -1,10 +1,13 @@
-from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.views import APIView
 from idea_jet_business.scripts.idea_gen import BusinessIdeaGeneration
 
 
 class BusinessIdeaView(APIView):
+
+    permission_classes = [IsAuthenticated]
 
     def post(self, *args, **kwargs):
         business_idea = BusinessIdeaGeneration().run()
