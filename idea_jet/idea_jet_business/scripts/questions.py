b7b4d22ff471e940aee46222310542fb81aa7326
zach <zach@web3technologies.io>
2023-05-21 16:10:29 -0500

    add lists questions and add update to landing page

---
+++ idea_jet/idea_jet_business/scripts/questions.py
@@ -0,0 +1,9 @@
+question_template = """
+
+    Given this business idea: {business_idea}
+
+    Generate a list of 5 questions that when given an answer will help you better understand the business.
+
+    YOUR RESPONSE:
+    
+"""
