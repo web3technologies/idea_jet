82063557de79e182e23d76e63ecccf87b18afa44
zach <zach@web3technologies.io>
2023-05-22 16:56:31 -0500

    added the login required to see the landing page

--- idea_jet/idea_jet/urls.py
+++ idea_jet/idea_jet/urls.py
@@ -3,11 +3,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
 
-from idea_jet_business.views import LandingPageView
+from idea_jet_business.views import LandingPageView, LoginView
 
 
 urlpatterns = [
     path('', LandingPageView.as_view(), name="landing_page_view"),
+    path('login/', LoginView.as_view(), name='login'),
     path('admin/', admin.site.urls),
     path("auth/", include("idea_jet_auth.urls")),
     path("business-idea/", include("idea_jet_business.urls"))
--- idea_jet/idea_jet_business/views/landing_page.py
+++ idea_jet/idea_jet_business/views/landing_page.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
-from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView
 
 
-class LandingPageView(View):
-    def get(self, request):
-        return render(request, 'idea_jet_business/index.html')
+class LandingPageView(LoginRequiredMixin, TemplateView):
+    template_name = 'idea_jet_business/index.html'
+    login_url = reverse_lazy('login')  # assuming 'login' is the name of your login url
+
+class LoginView(LoginView):
+    template_name = 'idea_jet_business/login.html'
+    redirect_authenticated_user = True  # If the user is authenticated, they'll be redirected to 'LOGIN_REDIRECT_URL'
