c67ee351e2eaef50e7f84df797fff6ec564ceec7
zach <zach@web3technologies.io>
2023-05-22 19:49:57 -0500

    correct the static files dir

--- idea_jet/idea_jet/settings/int.py
+++ idea_jet/idea_jet/settings/int.py
@@ -10,6 +10,6 @@ CORS_ORIGIN_WHITELIST = [
 MEDIA_ROOT = "/applications/idea_jet_media/"
 STATIC_ROOT = f"/applications/idea_jet/static/"   #collectstatic places all files found here in prod
 STATICFILES_DIRS = [
-    os.path.join("/applications/idea_jet/venv/lib/python/3.10/site-packages/", "idea_jet_static"),
+    os.path.join("/applications/idea_jet/", "venv/lib/python3.10/site-packages/idea_jet_static"),
     os.path.join("/applications/idea_jet/", "venv/lib/python3.10/site-packages/idea_jet_business/templates/idea_jet_business/")
 ]
