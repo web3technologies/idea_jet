fe5a6fd22c1f1bf4497866b5c3b6bfed6ed87d09
zach <zach@web3technologies.io>
2023-05-25 12:14:32 -0500

    update business serializer

--- idea_jet/idea_jet_business/serializers/business_idea.py
+++ idea_jet/idea_jet_business/serializers/business_idea.py
@@ -3,12 +3,15 @@ from rest_framework import serializers
 from idea_jet_business.models import BusinessIdea
 from idea_jet_business.serializers.execution_step import ExecutionStepSerializer
 from idea_jet_business.serializers.feature import FeatureSerializer
+from idea_jet_business.serializers.market_research import MarketResearchSerializer
 
 
 class BusinessIdeaSerializer(serializers.ModelSerializer):
 
     execution_steps = ExecutionStepSerializer(many=True, read_only=True)
     features = FeatureSerializer(many=True, read_only=True)
+    marketresearch_set = MarketResearchSerializer(many=True, read_only=True)
+
     business_model_type = serializers.SlugRelatedField(
         source="business_model", 
         slug_field='business_model_type', 
