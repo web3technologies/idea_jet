3eaf732bbf551c1a9149f05d779721c67c274b1e
zach <zach@web3technologies.io>
2023-06-02 22:41:32 -0500

    update settings

--- idea_jet/idea_jet/settings/int.py
+++ idea_jet/idea_jet/settings/int.py
@@ -16,25 +16,17 @@ STATICFILES_DIRS = [
     os.path.join("/applications/idea_jet/", "venv/lib/python3.10/site-packages/idea_jet_business/templates/idea_jet_business/")
 ]
 
-CELERY_WORKER_DISABLE_REMOTE_CONTROL = True
 CELERY_BROKER_URL = "sqs://"
 CELERY_TASK_DEFAULT_QUEUE = 'idea_jet_int'
-CELERY_TASK_DEFAULT_EXCHANGE = 'idea_jet_int'
-CELERY_TASK_DEFAULT_ROUTING_KEY = 'idea_jet_int'
-
 
 CELERY_BROKER_TRANSPORT_OPTIONS = {
     'region': 'us-east-2',
+    'is_secure': True,
     'predefined_queues': {
         'idea_jet_int': {
             'url': 'https://sqs.us-east-2.amazonaws.com/490305332793/idea_jet_int',
         },
-        # 'celery.pidbox': {
-        #     'url': 'https://sqs.us-east-2.amazonaws.com/490305332793/celery.pidbox',
-        # },
-        # Add other special queues as needed...
     },
     'visibility_timeout': 3600,
     'polling_interval': 5.0,
-    'queue_name_prefix': '',
 }
